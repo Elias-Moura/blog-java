896e8368bf94b08015127a7ed75bc3d5d17b5e59
Elias Moura <elias.smourayt@gmail.com>
2023-10-02 14:14:47 -0300

    Criando as rotas Put, Post e Delete

--- src/main/java/com/example/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/example/blogpessoal/repository/PostagemRepository.java
@@ -2,7 +2,11 @@ package com.example.blogpessoal.repository;
 
 import com.example.blogpessoal.models.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 
 public interface PostagemRepository extends JpaRepository<Postagem,Long> {
+    List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 }
