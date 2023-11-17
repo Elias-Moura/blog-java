4d71d856ab7b7159c7796ab87ac40eade29ec4ec
Elias Moura <elias.smourayt@gmail.com>
2023-11-17 10:16:12 -0300

    Não é mais necessário.

--- src/main/java/com/example/blogpessoal/domain/dto/tema/DadosTema.java
+++
@@ -1,9 +0,0 @@
-package com.example.blogpessoal.domain.dto.tema;
-
-import com.example.blogpessoal.domain.modelos.Tema;
-
-public record DadosTema(Long id) {
-    public DadosTema(Tema dados){
-        this(dados.getId());
-    }
-}
