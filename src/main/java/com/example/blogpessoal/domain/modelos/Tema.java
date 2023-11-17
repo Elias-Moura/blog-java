e16a609d50bcf7e2f4cbc95f98349f9e53f0afc4
Elias Moura <elias.smourayt@gmail.com>
2023-11-17 09:53:56 -0300

    Ajustes para declarar para o spring utilizar o soft delete.

--- src/main/java/com/example/blogpessoal/domain/modelos/Tema.java
+++ src/main/java/com/example/blogpessoal/domain/modelos/Tema.java
@@ -2,17 +2,17 @@ package com.example.blogpessoal.domain.modelos;
 
 import com.example.blogpessoal.domain.dto.tema.DadosAtualizacaoTema;
 import com.example.blogpessoal.domain.dto.tema.DadosCadastroTema;
-import com.example.blogpessoal.domain.dto.tema.DadosTema;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.*;
 
+import java.util.Collections;
 import java.util.List;
 
 
@@ -21,29 +21,39 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@SQLDelete(sql = "UPDATE tb_temas SET ativo = false WHERE id=?")
+@FilterDef(name = "deletedTemaFilter", parameters = @ParamDef(name="isDeleted", type=Boolean.class))
+@Filter(name="deletedTemaFilter", condition="ativo = :isDeleted")
 @EqualsAndHashCode(of = "id")
 public class Tema {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String titulo;
+
     private String descricao;
+
     @OneToMany(mappedBy = "tema", cascade= CascadeType.REMOVE)
-    private List<Postagem> postagems;
+    @JsonIgnoreProperties("tema")
+    private List<Postagem> postagens;
+
     private Boolean ativo;
 
-    public Tema(DadosTema tema) {
+    public Tema(DadosAtualizacaoTema tema) {
         this.id = tema.id();
-    }
-
-    public Tema(DadosCadastroTema tema) {
         this.descricao = tema.descricao();
     }
 
+    public Tema(DadosCadastroTema tema) {
+        this.titulo = tema.titulo();
+        this.descricao = tema.descricao();
+        this.postagens = Collections.emptyList();
+        this.ativo = true;
+    }
+
     public void atualizarInformacoes(DadosAtualizacaoTema dados) {
+        this.titulo = dados.titulo();
         this.descricao = dados.descricao();
     }
 
-    public void excluir() {
-        this.ativo = false;
-    }
 }
